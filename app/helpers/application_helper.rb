c58a79ab0b69ea9388be30b686909dbc6337bbca
shiro <shiro.mns@gmail.com>
2018-11-15 17:35:09 +0900

    [修正]タイトルの変更忘れ

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
     def page_title
-        title = "戯書1期データ小屋"
+        title = "戯書2期データ小屋"
         title = @page_title + " | " + title if @page_title
         title
     end
