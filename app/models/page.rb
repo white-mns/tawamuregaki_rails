26d4568886cb07d5d77629923f01bab2a4e123c3
shiro <shiro.mns@gmail.com>
2025-11-15 23:35:40 +0900

    [更新]Ransack 2.4.2 -> 4.3.0

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,4 +1,13 @@
 class Page < ApplicationRecord
+
+    def self.ransackable_attributes(auth_object = nil)
+      column_names
+    end
+
+    def self.ransackable_associations(auth_object = nil)
+      Array(reflect_on_all_associations).map(&:name).map(&:to_s)
+    end
+
 	belongs_to :story, :foreign_key => :story_id, :primary_key => :story_no, :class_name => 'StoryDatum'
 	has_many :party_members, -> {order('party_order ASC')}, :foreign_key => :battle_no, :primary_key => :battle_no, :class_name => 'Party'
 	has_many :enemy_members, :foreign_key => :battle_no, :primary_key => :battle_no, :class_name => 'Enemy'
@@ -7,21 +16,21 @@ class Page < ApplicationRecord
 
     scope :where_members, -> (params) {
         if params["party_members_e_no_form"] || params["party_members_name_form"] then
-            query = Party.includes(:pc_name).search(params[:q]).result.select(:battle_no)
+            query = Party.includes(:pc_name).ransack(params[:q]).result.select(:battle_no)
             where(battle_no: query)
         end
     }
 
     scope :where_leader, -> (params) {
         if params["leader_e_no_form"] || params["leader_name_form"] then
-            query = Party.includes(:pc_name).search(params[:q]).result.select(:battle_no)
+            query = Party.includes(:pc_name).ransack(params[:q]).result.select(:battle_no)
             where(battle_no: query)
         end
     }
 
     scope :where_fellows, -> (params) {
         if params["fellow_members_e_no_form"] || params["fellow_members_name_form"] then
-            query = Party.includes(:pc_name).search(params[:q]).result.select(:battle_no)
+            query = Party.includes(:pc_name).ransack(params[:q]).result.select(:battle_no)
             where(battle_no: query)
         end
     }
